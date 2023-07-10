941b807adefea6bf42a28aa18ac3f86b3647075d
ls3020 <ls3020@ic.ac.uk>
2023-07-10 11:30:38 +0100

    pacman with collider

--- Assets/Scripts/AnimatingPacman.cs
+++ Assets/Scripts/AnimatingPacman.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class AnimatingPacman : MonoBehaviour
 {
+    public int MouthSpeed=3;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) ||Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) )
+        {
+            Debug.Log("in loop");
+            gameObject.transform.localEulerAngles = new Vector3(0f, 180 + Mathf.Sin(Time.time*MouthSpeed)*30, 0f);
+        }
     }
 }
