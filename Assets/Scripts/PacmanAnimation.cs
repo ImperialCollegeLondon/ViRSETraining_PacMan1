8c83706d943e7997808e1f5cb8e0b6a444c71f75
KirelianWolf <v.kirensky@gmail.com>
2023-07-10 10:48:00 +0100

    deleted current scripts, added script for Pacman movement and animation

--- Assets/Scripts/PacmanAnimation.cs
+++ Assets/Scripts/PacmanAnimation.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scriptlola : MonoBehaviour
+public class PacmanAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("script1 works x");
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/PacmanMovement.cs
+++ Assets/Scripts/PacmanMovement.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZacScript : MonoBehaviour
+public class PacmanMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello.");
+        
     }
 
     // Update is called once per frame
