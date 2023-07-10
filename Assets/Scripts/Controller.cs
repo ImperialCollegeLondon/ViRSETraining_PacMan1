6353be796e950bb47d9a486dc067dd75e49bb6ca
KirelianWolf <v.kirensky@gmail.com>
2023-07-10 15:35:26 +0100

    Update Controller.cs

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,54 +6,33 @@ using DG.Tweening;
 
 public class Controller : MonoBehaviour
 {
-    public float speed = 1;
+    private float speed = 0.2f;    // the bigger, the smaller the speed
     public static Controller instance;
     public Vector3 currentDirection = Vector3.zero;
     private Vector3 nextDirection = Vector3.zero;
 
     private int wall;
 
-    public int minX = -4;
-    public int maxX = 4;
-    public int minZ = -4;
-    public int maxZ = 4;
-
-    private float dx;
-    private float dz;
-
     private void Awake()
     {
         instance = this;
-        
     }
-    // Start is called before the first frame update
     void Start()
     {
         Move();
         wall = LayerMask.GetMask("Wall");
-        dx = maxX - minX;
-        dz = maxZ - minZ;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
             nextDirection = Vector3.forward;
-        }
         if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
             nextDirection = Vector3.back;
-        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
             nextDirection = Vector3.left;
-        }
         if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
             nextDirection = Vector3.right;
-        }
     }
 
     void Move()
@@ -72,28 +51,23 @@ public class Controller : MonoBehaviour
             transform.LookAt(moveTo);
         }
 
+
         // Teleport
-        if (moveTo.x < minX)
+        if (gameObject.transform.position.x < -14.5f)
         {
-            transform.position += new Vector3(dx, 0, 0);
-            moveTo += new Vector3(dx, 0, 0);
-        }
-        if (moveTo.x > maxX)
-        {
-            transform.position -= new Vector3(dx, 0, 0);
-            moveTo -= new Vector3(dx, 0, 0);
-        }
-        if (moveTo.z < minZ)
-        {
-            transform.position += new Vector3(0, 0, dz);
-            moveTo += new Vector3(0, 0, dz);
-        }
-        if (moveTo.z > maxZ)
-        {
-            transform.position -= new Vector3(0, 0, dz);
-            moveTo -= new Vector3(0, 0, dz);
+            //speed = 0.01f;
+            moveTo = new Vector3(14, 1, 3);
+            gameObject.transform.position = new Vector3(15, 1, 3);
+            gameObject.GetComponent<Rigidbody>().position = new Vector3(15, 1, 3);
         }
 
-        transform.DOMove(moveTo, 1 / speed).SetEase(Ease.Linear).OnComplete( () => Move() );
+        if (gameObject.transform.position.x > 15.5f)
+        {
+            //speed = 0.01f;
+            moveTo = new Vector3(-13, 1, -7);
+            gameObject.transform.position = new Vector3(-14, 1, -7);
+            gameObject.GetComponent<Rigidbody>().position = new Vector3(-14, 1, -7);
+        }
+        transform.DOMove(moveTo, speed).SetEase(Ease.Linear).OnComplete( () => Move() );
     }
 }
