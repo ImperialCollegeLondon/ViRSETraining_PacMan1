a7340a48975caea3a7dd0712d58ac12afeb9b86c
ZacharyWu131 <zw2921@ic.ac.uk>
2024-07-08 15:05:21 +0100

    lives object done

--- Assets/Scripts/PacmanHeadMovement/ScoreController.cs
+++ Assets/Scripts/PacmanHeadMovement/ScoreController.cs
@@ -11,6 +11,16 @@ public class ScoreController : MonoBehaviour
     public TMP_Text add_score_world;
 
     private float current_score = 0;
+    private float current_live = 3;
+
+    public GameObject heart3;
+    public GameObject heart2;
+    public GameObject heart1;
+
+    public GameObject heart3_w;
+    public GameObject heart2_w;
+    public GameObject heart1_w;
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -28,10 +38,31 @@ public class ScoreController : MonoBehaviour
         add_score_world.text = ($"{current_score}");
         //Debug.Log(current_score);
 
+        if (current_live == 2)
+        {
+            heart3.SetActive( false );
+            heart3_w.SetActive(false);
+        }
+        if (current_live == 1)
+        {
+            heart2.SetActive(false);
+            heart2_w.SetActive(false);
+        }
+        if (current_live == 0)
+        {
+            heart1.SetActive(false);
+            heart1_w.SetActive(false);
+        }
+
     }
 
     public void Score(float score)
     {
         current_score += score;
+    }
+
+    public void Lives()
+    {
+        current_live--;
     }
 }
