2bbfa7694b4c8aa8395e04a9036f1e942579a35d
ZacharyWu131 <zw2921@ic.ac.uk>
2024-07-09 12:23:29 +0100

    both end plate done

--- Assets/Scripts/PacmanHeadMovement/ScoreController.cs
+++ Assets/Scripts/PacmanHeadMovement/ScoreController.cs
@@ -11,6 +11,8 @@ public class ScoreController : MonoBehaviour
     public TMP_Text add_score_world;
     public TMP_Text end_score;
     public TMP_Text end_board_text;
+    public TMP_Text end_score2;
+    public TMP_Text end_board_text2;
 
     private float current_score = 0;
     private float current_live = 3;
@@ -24,7 +26,8 @@ public class ScoreController : MonoBehaviour
     public GameObject heart1_w;
 
     public GameObject end_board;
-    private int pill_no = 362;
+    public GameObject end_board2;
+    private int pill_no = 360;
     private bool game_end = false;
 
     // Start is called before the first frame update
@@ -35,6 +38,7 @@ public class ScoreController : MonoBehaviour
     void Start()
     {
         end_board.SetActive(false);
+        end_board2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -57,20 +61,26 @@ public class ScoreController : MonoBehaviour
         if (current_live == 0 && game_end == false)
         {
             end_board.SetActive(true);
+            end_board2.SetActive(true);
             heart1.SetActive(false);
             heart1_w.SetActive(false);
             game_end = true;
             end_score.text = ($"{current_score}");
             end_board_text.text = ("You Lose.");
+            end_score2.text = ($"{current_score}");
+            end_board_text2.text = ("You Lose.");
         }
 
 
         if (pill_no == 0 && game_end == false) 
         {
             end_board.SetActive(true);
+            end_board2.SetActive(true);
             game_end = true;
             end_board_text.text = ("You Win!");
             end_score.text = ($"{current_score}");
+            end_board_text2.text = ("You Win!");
+            end_score2.text = ($"{current_score}");
         }
     }
 
